0d2c2f21d07ab31aaa0eced4965bb1d8a6dac9f6
PRASHANT VERMA <vermapv749@gmail.com>
2024-10-21 12:19:42 +0530

    multiplication function add

---
+++ multi.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a , b;
+    cout<<"Enter the first Number :";
+    cin>>a;
+    cout<<"enter the sum number :";
+    cin>>b;
+    cout<<"multiplication of both the number is :"<<a*b<<endl;
+}
